eb496cac62c870d78608c989db4b9382000b434b
milansir <rwn3developer11@gmail.com>
2024-03-21 13:46:20 +0530

    topbar-done

--- facebookclone/client/src/component/topbar/Topbar.jsx
+++ facebookclone/client/src/component/topbar/Topbar.jsx
@@ -1,41 +1,34 @@
 import React from 'react'
 import './topbar.css'
+import { Link } from 'react-router-dom'
 const Topbar = () => {
     return (
-        <div className='topbar'>
-            <div className='container-fluid'>
-                <div className="row bg-dark p-2">
-                    <div className="col-md-4">
-                        <span style={{ fontSize: "30px", fontWeight: "bold", color: "white" }}>Social Media</span>
-                    </div>
-                    <div className="col-md-4 d-flex">
-                        <input type='text' className='form-control w-50' /><button className='btn btn-success'>Submit</button>
-                    </div>
-                    <div className="col-md-4">
-                        <nav className="navbar navbar-expand-sm bg-warning navbar-dark">
-                            <div className="container-fluid">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <a className="nav-link active" href="#">Active</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link disabled" href="#">Disabled</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
 
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+                <a className="navbar-brand" href="/">My Social Media</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <input type='text' placeholder='search post' className='form-control w-50'/> <button style={{marginLeft : "15px"}} className='btn btn-warning btn-sm'>submit</button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/profile">Profile</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/settings">Settings</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </nav>
 
-        </div>
     )
 }
 
